847352954b8e3d71d943bdfbd0ca4ab82af14589
Patrick Gallagher <pgallagher85@gmail.com>
2017-11-14 00:49:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function theBeatlesPlay (musicians, instruments) {
   var array = []
-  for (let i === 0, i < 4, i++) {
+  for (let i = 0, i < 4, i++) {
     array.push('${musicians} plays ${instruments}')
   }
   return array
